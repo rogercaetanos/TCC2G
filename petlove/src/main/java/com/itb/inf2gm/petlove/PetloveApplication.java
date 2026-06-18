e2c305a7feac077f8b5c8ae897473b8a63e6f36a
Rogério Caetano dos Santos <rogerio.santos@docente.fieb.edu.br>
2026-06-18 07:41:37 -0300

    Mensagem inicialização server

--- petlove/src/main/java/com/itb/inf2gm/petlove/PetloveApplication.java
+++ petlove/src/main/java/com/itb/inf2gm/petlove/PetloveApplication.java
@@ -8,6 +8,7 @@ public class PetloveApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetloveApplication.class, args);
+		System.out.println("Server start port 8080");
 	}
 
 }
