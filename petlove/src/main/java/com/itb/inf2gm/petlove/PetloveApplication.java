3051101fb2e7c67e2cecf2e4113e0e0e52ea07e5
Rogério Caetano <rogercaetanos@gmail.com>
2026-06-18 10:33:38 +0000

    Criação do projeto Spring Boot

---
+++ petlove/src/main/java/com/itb/inf2gm/petlove/PetloveApplication.java
@@ -0,0 +1,13 @@
+package com.itb.inf2gm.petlove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetloveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetloveApplication.class, args);
+	}
+
+}
